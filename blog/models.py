77f70cd40c42a59fa8cc2721292d3f1ed84c4119
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2022-10-03 13:19:05 +0500

    update

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField(max_length=200)
+    description = models.TextField(max_length=20000)
     date = models.DateField()
 
     def __str__(self):
